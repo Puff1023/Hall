323ae910ea9a8d2930c8c5884ca278747137f813
LAPTOP-E0FBB1VO\zxcvb <zxcvbnm314223@gmail.com>
2022-03-30 22:30:16 +0800

    牆上物件、神明廳、電梯排好的

--- Assets/Script/Trigger.cs
+++ Assets/Script/Trigger.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class Trigger : MonoBehaviour
 {
     public Transform Entrance;//入口牆
-    public Transform Gear;//阻擋牆
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             Debug.Log("入口堵住");
-            Entrance.position = new Vector3(12.85904f, 1.8375f, 15.32989f);
-            Gear.position = new Vector3(0, 0, 0);
+            Entrance.position = new Vector3(-92.85992f, 0, -25.13997f);
         }
     }
 }
